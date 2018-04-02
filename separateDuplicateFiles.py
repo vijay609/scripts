0b46063d1883feb21f02f04aef52fed308027497
Vijay Baiyya <vijay.609@gmailcom>
2018-04-02 06:58:41 -0700

    Fine tuning perf + comments

--- separateDuplicateFiles.py
+++ separateDuplicateFiles.py
@@ -1,12 +1,14 @@
-## Start by loading the knownFiles list
-## Prepare the list of known file paths
-## Find duplicates using the hash
-## move duplicates to duplicates folder
-## the run should output [hash, originalFilePath, [duplciate 1 path, /duplicate 2 path, duplicate 3,....]]
-## save this knownFiles list along with the hash [hash, original file path]
+'''
+Start by loading the knownFiles list
+Prepare the list of known file paths
+Find duplicates using the hash
+move duplicates to duplicates folder
+the run should output [hash, originalFilePath, [duplciate 1 path, /duplicate 2 path, duplicate 3,....]]
+save this knownFiles list along with the hash [hash, original file path]
+'''
+
 import json
 from os import walk, path, sep, makedirs
-
 import concurrent.futures
 import hashlib
 import time
@@ -18,7 +20,6 @@ import cProfile
 import pprint
 makeDirLock = threading.Lock()
 
-
 class File:
     def __init__(self, original, duplicates = []):
         self._original = original
@@ -27,8 +28,10 @@ class File:
     def addDuplicate(self, duplicate):
         self._duplicates.append(duplicate)
 
-## This method will get called for json deserialization for every json block
-## from innermost level to outermost level.
+'''
+This method will get called for json deserialization for every json block
+from innermost level to outermost level.
+'''
 def createFromJson(jsonObject):
     if '_original' in jsonObject and '_duplicates' in jsonObject :
         f = File(jsonObject['_original'])
@@ -42,8 +45,10 @@ def createFromJson(jsonObject):
     else:
         raise JSONDecodeError(msg='unexpected json', doc=json.dumps(jsonObject, default=lambda o : o.__dict__), pos=0)
 
-## loads the file paths and hashes that are previously known
-## also ensures that all those files are actually present on the disk
+'''
+loads the file paths and hashes that are previously known
+also ensures that all those files are actually present on the disk
+'''
 def loadKnownFilesMap(knownFilesMapFile):
     filesMap = {}
     with open(knownFilesMapFile) as fh:
@@ -59,8 +64,10 @@ def loadKnownFilesMap(knownFilesMapFile):
 
     return filesMap
 
-## creates and returns a list of all the file paths from the inputPaths that are not
-## known(not present in knownFilesMap)
+'''
+creates and returns a list of all the file paths from the inputPaths that are not
+known(not present in knownFilesMap)
+'''
 def buildInputFilesList(inputPaths, knownFilesMap):
     if type(inputPaths) is not list:
         raise TypeError("buildInputFilesList expects a list of input paths")
@@ -73,36 +80,47 @@ def buildInputFilesList(inputPaths, knownFilesMap):
         if not path.exists(inputPath):
             raise OSError("Invalid input folder path : {}".format(inputPath))
         for root, dirs, files in walk(inputPath):
-            # Within a directory, sort the files first by filename length and then by full filename
-            # This helps us put 'img2025.png' before 'copy of img2025.png'
+            '''Within a directory, sort the files first by filename length and then by full filename
+            This helps us put 'img2025.png' before 'copy of img2025.png'
+            '''
             files.sort(key=lambda x : (len(path.split(x)[-1]), path.split(x)[-1]))
             inputFiles.extend(img for img in [path.join(root, f) for f in files] if img not in inputFiles if img not in knownFiles)
-            # Although dirs is not used directly, sorting the list of dirs here will make
-            # os.walk to parse the directories in that order
+            '''
+            Although dirs is not used directly, sorting the list of dirs here will make
+            os.walk to parse the directories in that order
+            '''
             dirs.sort()
 
     return inputFiles
 
-## Calculates the MD5 hash of a file
+'''
+Calculates the MD5 hash of a file
+'''
 def calculateMD5Hash(file):
     with open(file, "rb") as fh:
         return hashlib.md5(fh.read()).hexdigest()
 
-## Given an input fileTupleList [(id3, file3), (1d1, file1), (id2, file2), (id0, file0)] for example,
-## return file0, [file1, file2, file3]
+'''
+Given an input fileTupleList [(id3, file3), (1d1, file1), (id2, file2), (id0, file0)] for example,
+return file0, [file1, file2, file3]
+'''
 def getOriginalAndDuplicates(fileTupleList):
     fileTupleList.sort()
     return fileTupleList[0][1], [x[1] for x in fileTupleList[1:]]
 
-## find all the inputFiles that are already present in knownFilesMap and add them to the duplicateFilesMap
-## all the new files frim teh inputFiles get added to knownFilesMap
+'''
+find all the inputFiles that are already present in knownFilesMap and add them to the duplicateFilesMap
+all the new files frim teh inputFiles get added to knownFilesMap
+'''
 def checkForDuplicates(inputFiles, filesMap):
     hashToFiles = {}
     # We can use a with statement to ensure threads are cleaned up promptly
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        # In order to preserve the order of the inputFiles we use enumerate(inputFiles) which returns tuples
-        # of the form (index, inputFile) and combine that with the hash of the input file into a single tuple
-        for (index, inputFile), md5Hash in zip(enumerate(inputFiles), executor.map(calculateMD5Hash, inputFiles)):
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        '''In order to preserve the order of the inputFiles we use enumerate(inputFiles) which returns tuples
+        of the form (index, inputFile) and combine that with the hash of the input file into a single tuple
+        '''
+        # optimal number of chunks should be same as the number of processors/cores (12 in our case)
+        for (index, inputFile), md5Hash in zip(enumerate(inputFiles), executor.map(calculateMD5Hash, inputFiles, chunksize=len(inputFiles)//12)):
             if md5Hash in filesMap:
                 filesMap[md5Hash].addDuplicate(inputFile)
 
@@ -112,12 +130,14 @@ def checkForDuplicates(inputFiles, filesMap):
             else:
                 hashToFiles[md5Hash] = [(index, inputFile)]
 
-    # At this point we have files separated by hashes. The hashToFiles dictionary has md5Hash as the key
-    # and a list of tuples (index, file) as value. Since which thread in the threadpool returns first is
-    # not deterministic, we can use the index of the file for sorting each list in hashToFiles and pick
-    # the top one (after sorting) as the original
+    '''
+    At this point we have files separated by hashes. The hashToFiles dictionary has md5Hash as the key
+    and a list of tuples (index, file) as value. Since which thread in the threadpool returns first is
+    not deterministic, we can use the index of the file for sorting each list in hashToFiles and pick
+    the top one (after sorting) as the original'''
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        for md5Hash, (original, duplicates) in zip (hashToFiles.keys(), executor.map(getOriginalAndDuplicates, hashToFiles.values())):
+        for md5Hash, (original, duplicates) \
+            in zip (hashToFiles.keys(), executor.map(getOriginalAndDuplicates, hashToFiles.values())):
             assert (md5Hash not in filesMap.keys())
             filesMap[md5Hash] = File (original, duplicates)
 
@@ -140,10 +160,12 @@ def moveFile(srcFile, destFile):
         logging.exception("")
         raise
 
-## moves all the duplicates from the filesMap to duplicatesDestRoot folder and preserves
-## the original directory structure
+'''
+moves all the duplicates from the filesMap to duplicatesDestRoot folder and preserves
+the original directory structure
+'''
 def moveTheDuplicates(filesMap, duplicatesDestRoot):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ProcessPoolExecutor() as executor:
         moveFileFutures = {}
         for v in filesMap.values():
             for i in range(len(v._duplicates)):
