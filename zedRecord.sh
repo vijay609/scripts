5e46ff79a529b07d91c551f04503717c6abdb232
Vijay Baiyya <vijay.609@gmailcom>
2018-07-18 07:18:16 -0700

    * Move files around * Remove unused files

--- zedRecord.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-## $1 = <path to the .svo file/recording>
-
-CONTAINER_NAME=zedDevEnvironment
-
-docker exec -it ${CONTAINER_NAME} /bin/bash -c "/src/zed-examples/svo\ recording/recording/build/ZED_SVO_Recording ${1}"
